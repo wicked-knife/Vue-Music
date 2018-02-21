dc90379626c91fad0c3ef994c4c8f4d7db625ce1
ghostratel <1353640486@qq.com>
2018-02-21 12:29:40 +0800

    nothing

--- src/components/M-Header/M-Header.vue
+++ src/components/M-Header/M-Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="m-header">
-      <img src="../../common/image/logo2.png" id="app-logo">
-      <h1 id="app-name">波斯音乐</h1>
+      <img src="../../common/image/logo.png" id="app-logo">
+      <h1 id="app-name">chicken音乐</h1>
       <router-link to="/center" tag='span'>
             <i class="iconfont icon-people"></i>
       </router-link>
@@ -22,7 +22,7 @@ export default {
     color:$color-theme;
     overflow: hidden;
     #app-logo{
-      width:60px;height: 32px;
+      width:32px;height: 32px;
       position: relative;
       top:7px;
       margin-left: 30px;
